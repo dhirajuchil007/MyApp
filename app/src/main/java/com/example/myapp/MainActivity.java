1ffe401b3ff01c13697ebb9f3cda4a84427866f2
dhirajuchil00 <dhiraj@cashrich.com>
2020-02-19 23:03:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final String country[]={"India","US","UK"};
+
+        ListView list=findViewById(R.id.country_list);
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,R.layout.country_list_item
+        ,R.id.text,country);
+
+        list.setAdapter(arrayAdapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this,country[i],Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
